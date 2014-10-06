5f058b8f88c61e3b04078f31592b33fe0e41ee76
rbaertsch <baertsch@soe.ucsc.edu>
2014-10-05 20:53:15 -0700

    add todos scaffolding and initial publish method

---
+++ client/views/todos/todos_index/todos_index.js
@@ -0,0 +1,35 @@
+
+/*****************************************************************************/
+/* TodosIndex: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.TodosIndex.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.TodosIndex.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* TodosIndex: Lifecycle Hooks */
+/*****************************************************************************/
+Template.TodosIndex.created = function () {
+};
+
+Template.TodosIndex.rendered = function () {
+};
+
+Template.TodosIndex.destroyed = function () {
+};
+
+
---
+++ server/collections/todos.js
@@ -0,0 +1,32 @@
+/*
+ * Add query methods like this:
+ *  Todos.findPublic = function () {
+ *    return Todos.find({is_public: true});
+ *  }
+ */
+
+Todos.allow( {
+	insert: function( userId, doc ){
+		return userId;	
+	},
+	update: function( userId, doc, fieldNames, modifier) {
+		return userId === doc.user_id;
+	},
+	remove: function (userId, doc) {
+		return userId === doc.user_id;
+	}
+});
+
+Todos.deny({
+  insert: function (userId, doc) {
+    return false;
+  },
+
+  update: function (userId, doc, fieldNames, modifier) {
+    return false;
+  },
+
+  remove: function (userId, doc) {
+    return false;
+  }
+});
